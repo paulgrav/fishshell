737f609d9af6ec194b2b4a8bfe0038528ea4f134
Paul Grave <paul.grave@sky.uk>
2016-06-28 12:16:52 +0100

    changed: vi_mode deprecated in favour of vi_key_bindings

--- config.fish
+++ config.fish
@@ -1,18 +1,25 @@
-fish_vi_mode
+fish_vi_key_bindings
 
-function dm
-    docker-machine $argv
-end
+alias dm "docker-machine"
+alias dc "docker-compose"
+alias bb "bbedit"
 
 # Fish git prompt
 set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showstashstate 'yes'
 set __fish_git_prompt_showupstream 'yes'
 set __fish_git_prompt_color_branch yellow
-                                                                                                                                                                                                  
+
 # Status Chars
 set __fish_git_prompt_char_dirtystate '⚡'
 set __fish_git_prompt_char_stagedstate '→'
 set __fish_git_prompt_char_stashstate '↩'
 set __fish_git_prompt_char_upstream_ahead '↑'
 set __fish_git_prompt_char_upstream_behind '↓'
+
+set -x PATH $PATH /usr/local/heroku/bin $HOME/.bin
+
+for x in ~/.config/fish/conf.d/*.fish
+	source $x
+end
+
