e45bb0c34f473a7d2de8242af5ec79bb77cb7ca3
Paul Grave <paul.grave@sky.uk>
2020-04-06 16:59:59 +0100

    improved: prompt with more info

--- config.fish
+++ config.fish
@@ -3,23 +3,23 @@ fish_vi_key_bindings
 alias dm "docker-machine"
 alias dc "docker-compose"
 alias bb "bbedit"
+alias k "kubectl"
+alias mk "minikube"
 
-# Fish git prompt
-set __fish_git_prompt_showdirtystate 'yes'
-set __fish_git_prompt_showstashstate 'yes'
-set __fish_git_prompt_showupstream 'yes'
-set __fish_git_prompt_color_branch yellow
-
-# Status Chars
-set __fish_git_prompt_char_dirtystate '⚡'
-set __fish_git_prompt_char_stagedstate '→'
-set __fish_git_prompt_char_stashstate '↩'
-set __fish_git_prompt_char_upstream_ahead '↑'
-set __fish_git_prompt_char_upstream_behind '↓'
+set -x PATH $PATH /usr/local/sbin
 
 if test -d /usr/local/heroku/bin
     set -x PATH $PATH /usr/local/heroku/bin
 end
+
+if test -d /Users/pgrave/.cargo/bin
+    set -x PATH $PATH /Users/pgrave/.cargo/bin
+end
+
+if test -d /Users/pgrave/google-cloud-sdk/bin
+    set -x PATH $PATH /Users/pgrave/google-cloud-sdk/bin
+end
+
 
 if test -d $HOME/.bin
     set -x PATH $PATH $HOME/.bin
@@ -29,3 +29,8 @@ for x in ~/.config/fish/conf.d/*.fish
 	source $x
 end
 
+
+
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/usr/local/google-cloud-sdk/path.fish.inc' ]; if type source > /dev/null; source '/usr/local/google-cloud-sdk/path.fish.inc'; else; . '/usr/local/google-cloud-sdk/path.fish.inc'; end; end
